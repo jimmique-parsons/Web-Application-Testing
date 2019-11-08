49029119d04aa4e9674efc314c0ab65bb4b791fd
Jimmique <jimmique88@gmail.com>
2019-11-07 20:43:53 -0800

    Added Component files, set up a render test, added scoring functions

--- web-app-test/src/App.js
+++ web-app-test/src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';s
 import './App.css';
+import Dashboard from './components/Dashboard';
+import Display from '.components/Display';
+
+ export const addBall = balls => balls + 1;
+ export const addStrike = strikes => strikes + 1;
+ export const addFoul = fouls => fouls + 1;
+ export const addHit = hits => hits + 1;
 
 function App() {
+  const [ balls, setBalls ] = useState(0);
+  const [ strikes, setStrikes ] = useState(0);
+  const [ fouls, setFouls ] = useState(0);
+  const [ hits, setHits ] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Dashboard props={{
+        balls,
+        setBalls,
+        strikes,
+        setStrikes,
+        fouls,
+        setFouls,
+        hits,
+        setHits
+      }}/>
+      <Display props={{
+        balls,
+        setBalls,
+        strikes,
+        setStrikes,
+        fouls,
+        setFouls,
+        hits,
+        setHits
+      }}/>
     </div>
   );
 }
